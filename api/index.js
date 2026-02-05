21aecf77eb1599489f70761703b01a9644f42b31
Renee Matthew <renee.matthew04@gmail.com>
2026-02-05 07:13:14 +0530

    Configure for Vercel: Merge deps and add API adapter

---
+++ api/index.js
@@ -0,0 +1,6 @@
+// Vercel Serverless Function Adapter for Express
+// This file delegates the request to the main server entrypoint
+
+const app = require('../server/index.js');
+
+module.exports = app;
